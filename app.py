3cae2b1bfb98f03867a4a3e267fd982b29d592aa
Zaid De anda <zaidthejedi@gmail.com>
2024-07-09 16:28:26 -0600

    checks for user before showing pages

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ st.set_page_config(layout="wide")
 if check_password():
 
     user = get_user()
-    pages = {
-        "Mapa" : [
-            st.Page("vistas/mapa.py", title="Mapa")
-        ],
-        "Vistas" : [
-            st.Page("vistas/vista.py", title="Vista matriz"),
-            st.Page("vistas/buscador.py", title="Buscador de curps")
-        ]
-    }
+    if user:
+        pages = {
+            "Mapa" : [
+                st.Page("vistas/mapa.py", title="Mapa")
+            ],
+            "Vistas" : [
+                st.Page("vistas/vista.py", title="Vista matriz"),
+                st.Page("vistas/buscador.py", title="Buscador de curps")
+            ]
+        }
 
-    pg = st.navigation(pages)
-    pg.run()
+        pg = st.navigation(pages)
+        pg.run()
