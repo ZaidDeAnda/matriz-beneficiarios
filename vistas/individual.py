067f7d107c16f4b01bcff42bd8b49906244f742e
Zaid De anda <zaidthejedi@gmail.com>
2024-07-15 14:30:08 -0600

    reflect new changes

--- vistas/individual.py
+++ vistas/individual.py
@@ -13,7 +13,7 @@ total_df = load_generic_data_non_dummy(user=user, limit=2000)
 
 st.dataframe(total_df)
 with st.form("my_form", clear_on_submit=False):
-    submit = st.form_submit_button("Descargar BD", on_click=download_df, kwargs={"table" : user})
+    submit = st.form_submit_button("Descargar BD", on_click=download_df, kwargs={"user" : user})
 # try:
 # except:
 #     st.warning("Lo sentimos, la base de datos está presentando algunos fallos, estamos trabajando en ello ⚙ 👷‍♂️")
