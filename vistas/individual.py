c3537afa7f7e97fbfb53fc24bfb26657e916769c
Zaid De anda <zaidthejedi@gmail.com>
2024-10-14 10:28:31 -0600

    update to null data

--- vistas/individual.py
+++ vistas/individual.py
@@ -2,7 +2,7 @@ import streamlit as st
 from io import StringIO
 import pandas as pd
 
-from utils.data import download_df, load_generic_data_non_dummy
+from utils.data import download_df, load_generic_data_non_dummy, load_generic_null_data
 from utils.authentication import get_user, select_via
 
 st.header("Vista de usuarios por via")
@@ -18,7 +18,8 @@ st.dataframe(total_df, width=1400)
 with st.form("my_form", clear_on_submit=False):
     submit = st.form_submit_button("Descargar BD completa", on_click=download_df, kwargs={"user" : user})
 
-with st.form("null_data", clear_on_submit=False):
-    total_df = load_generic_data_non_dummy(user=user)
-    st.warning(f"Un total de {total_df.isnull().any(axis=1).sum()} registros cuentan con al menos un dato faltante")
-    st.download_button("Descargar datos incompletos ", data = total_df.isnull().any(axis=1).to_csv().encode("utf-8"), file_name="datos_incompletos.csv")
+download = st.button("Cargar datos incompletos")
+if download:
+    null_df = load_generic_null_data(user=user)
+    st.warning(f"Un total de {len(null_df)} registros cuentan con al menos un dato faltante")
+    st.download_button("Descargar datos incompletos ", data = null_df.to_csv().encode("utf-8"), file_name="datos_incompletos.csv")
